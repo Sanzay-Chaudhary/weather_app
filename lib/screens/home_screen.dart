337a8fd2788f01e03f7320bddba1294cad1096fa
Sanzay-Chaudhary <kctsanzu@gmail.com>
2024-05-31 09:32:56 +0545

    current data fetched

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/services/weather_service.dart';
@@ -10,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
   final WeatherService _weatherService = WeatherService();
   final String _city = "Bhairahawa";
   Map<String, dynamic>? _currentWeather;
@@ -19,9 +22,10 @@ class _HomeScreenState extends State<HomeScreen> {
     _fetchWeather();
   }
 
-  Future<void> _fetchWeather() async {
+  Future<void> _fetchWeather([String? city]) async {
     try {
-      final weatherData = await _weatherService.fetchCurrentWeather(_city);
+      final weatherData =
+          await _weatherService.fetchCurrentWeather(city ?? _city);
       setState(() {
         _currentWeather = weatherData;
       });
@@ -101,12 +105,126 @@ class _HomeScreenState extends State<HomeScreen> {
                               color: Colors.white,
                               fontWeight: FontWeight.bold),
                         ),
+                        Text(
+                          '${_currentWeather!['current']['condition']['text']}',
+                          style: GoogleFonts.lato(
+                              fontSize: 40,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              'Max: ${_currentWeather!['forecast']['forecastday'][0]['day']['maxtemp_c'].round()}°C',
+                              style: GoogleFonts.lato(
+                                  fontSize: 22,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Min: ${_currentWeather!['forecast']['forecastday'][0]['day']['mintemp_c'].round()}°C',
+                              style: GoogleFonts.lato(
+                                  fontSize: 22,
+                                  color: Colors.white70,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
                       ],
                     ),
+                  ),
+                  const SizedBox(
+                    height: 45,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildWeatherDetail(
+                          'Sunrise',
+                          Icons.wb_sunny,
+                          _currentWeather!['forecast']['forecastday'][0]
+                              ['astro']['sunrise']),
+                      _buildWeatherDetail(
+                          'Sunset',
+                          Icons.brightness_3,
+                          _currentWeather!['forecast']['forecastday'][0]
+                              ['astro']['sunset']),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildWeatherDetail('Humidity', Icons.opacity,
+                          _currentWeather!['current']['humidity']),
+                      _buildWeatherDetail('Wind(KPH)', Icons.wind_power,
+                          _currentWeather!['current']['wind_kph']),
+                    ],
                   ),
                 ],
               ),
             ),
+    );
+  }
+
+  Widget _buildWeatherDetail(String label, IconData icon, dynamic value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: AlignmentDirectional.topStart,
+                  end: AlignmentDirectional.bottomEnd,
+                  colors: [
+                    const Color(0xFF1A2344).withOpacity(0.5),
+                    const Color(0xFF1A2344).withOpacity(0.2),
+                  ],
+                )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.white,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  label,
+                  style: GoogleFonts.lato(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  value is String ? value : value.toString(),
+                  style: GoogleFonts.lato(
+                    fontSize: 18,
+                    color: Colors.white,
+                    //fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
