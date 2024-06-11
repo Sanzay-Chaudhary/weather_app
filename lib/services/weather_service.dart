3a4041d610bd4dbecd0c781558b93321bd11be08
Sanzay-Chaudhary <kctsanzu@gmail.com>
2024-06-12 00:54:39 +0545

    weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/model/weather_model.dart';
 
+//weather service to handle weather api
 class WeatherService {
   final String apiKey = "47d7bc01ca164a6699d15514243105";
   final String forecastBaseUrl = "http://api.weatherapi.com/v1/forecast.json";
